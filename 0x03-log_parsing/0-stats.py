4e86e7c43bd750f70c70bf5d22d486ac0d97bcfc
ZobaOkafor <judithokafor30@gmail.com>
2024-07-16 10:37:18 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,74 +2,41 @@
 """ script that reads stdin line by line and computes metrics """
 
 import sys
-import signal
 
-# Initialize variables
-total_size = 0
-status_codes_count = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+
+def print_metrics(status_counts, total_size):
+    """ Prints information """
+    print("File size: {:d}".format(total_size))
+    for code in sorted(status_counts.keys()):
+        if status_counts[code] != 0:
+            print("{}: {:d}".format(code, status_counts[code]))
+
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+                "404": 0, "405": 0, "500": 0}
+
 line_count = 0
-
-
-def print_stats():
-    """Prints the statistics"""
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes_count.keys()):
-        if status_codes_count[code] > 0:
-            print("{}: {}".format(code, status_codes_count[code]))
-
-
-def signal_handler(sig, frame):
-    """Handles the interrupt signal (CTRL + C)"""
-    print_stats()
-    sys.exit(0)
-
-
-# Register the signal handler
-signal.signal(signal.SIGINT, signal_handler)
+total_size = 0
 
 try:
-    for line in sys.stdin:
+    for log_line in sys.stdin:
+        if line_count != 0 and line_count % 10 == 0:
+            print_metrics(status_codes, total_size)
+
+        log_parts = log_line.split()
+        line_count += 1
+
         try:
-            parts = line.split()
-            if len(parts) < 7:
-                continue
-            ip = parts[0]
-            date = parts[3] + " " + parts[4]
-            request = parts[5] + " " + parts[6] + " " + parts[7]
-            status_code = parts[-2]
-            file_size = parts[-1]
+            total_size += int(log_parts[-1])
+        except:
+            pass
 
-            if not (status_code.isdigit() and file_size.isdigit()):
-                continue
-
-            status_code = str(status_code)
-            file_size = int(file_size)
-
-            # Update the total file size
-            total_size += file_size
-
-            # Update the status code count
-            if status_code in status_codes_count:
-                status_codes_count[status_code] += 1
-
-            line_count += 1
-
-            # Print statistics every 10 lines
-            if line_count % 10 == 0:
-                print_stats()
-
-        except Exception:
-            continue
+        try:
+            if log_parts[-2] in status_codes:
+                status_codes[log_parts[-2]] += 1
+        except:
+            pass
+    print_metrics(status_codes, total_size)
 
 except KeyboardInterrupt:
-    print_stats()
-    sys.exit(0)
+    print_metrics(status_codes, total_size)
+    raise
