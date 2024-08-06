b6accf657f183bb5bf4c5fab236be4fce24edbfe
ZobaOkafor <judithokafor30@gmail.com>
2024-08-06 10:07:16 -0700

    Star wars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,52 +2,33 @@
 
 const request = require('request');
 
-const filmId = process.argv[2];
-const apiEndpoint = `https://swapi-api.hbtn.io/api/films/${filmId}`;
-let characterUrls = [];
-const characterNames = [];
+const filmId = process.argv[2] + '/';
+const apiBase = 'https://swapi-api.hbtn.io/api/films/';
 
-const fetchFilmData = async () => {
-  await new Promise(resolve => request(apiEndpoint, (error, response, data) => {
-    if (error || response.statusCode !== 200) {
-      console.error('Error: ', error, '| StatusCode: ', response.statusCode);
-    } else {
-      const filmData = JSON.parse(data);
-      characterUrls = filmData.characters;
-      resolve();
-    }
-  }));
-};
+const fetchFilmCharacters = async () => {
+  try {
+    const filmUrl = apiBase + filmId;
+    const filmData = await new Promise((resolve, reject) => {
+      request(filmUrl, (error, response, body) => {
+        if (error) return reject(error);
+        resolve(JSON.parse(body));
+      });
+    });
 
-const fetchCharacterNames = async () => {
-  if (characterUrls.length > 0) {
-    for (const url of characterUrls) {
-      await new Promise(resolve => request(url, (error, response, data) => {
-        if (error || response.statusCode !== 200) {
-          console.error('Error: ', error, '| StatusCode: ', response.statusCode);
-        } else {
-          const characterData = JSON.parse(data);
-          characterNames.push(characterData.name);
-          resolve();
-        }
-      }));
+    const characterEndpoints = filmData.characters;
+
+    for (const endpoint of characterEndpoints) {
+      const characterData = await new Promise((resolve, reject) => {
+        request(endpoint, (error, response, body) => {
+          if (error) return reject(error);
+          resolve(JSON.parse(body).name);
+        });
+      });
+      console.log(characterData);
     }
-  } else {
-    console.error('Error: No character URLs found');
+  } catch (error) {
+    console.error(error);
   }
 };
 
-const printCharacterNames = async () => {
-  await fetchFilmData();
-  await fetchCharacterNames();
-
-  characterNames.forEach((name, index) => {
-    if (index === characterNames.length - 1) {
-      process.stdout.write(name);
-    } else {
-      process.stdout.write(name + '\n');
-    }
-  });
-};
-
-printCharacterNames();
+fetchFilmCharacters();
